70d39d38ce161a97ac09ef7c34c2d6fecab17866
Joelimiter <john.boehm36@gmail.com>
2022-02-05 19:07:32 +0100

    Add Axios Library  + 1 Function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+const API_URL = process.env.REACT_APP_BACKEND_URL
 
 function App() {
+
+  const message = axios.get( `${API_URL}test`).then(response => {
+    console.log(response)
+  })
+  
   return (
     <div className="App">
       <header className="App-header">
